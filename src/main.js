edfb296ce671f20f3f7f2ed7335398816bdb5df7
aj <arunjagadishsd@gmail.com>
2020-07-13 17:23:15 +0530

    Initialized vue

--- src/main.js
+++ src/main.js
@@ -2,11 +2,29 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueSocketIO from "vue-socket.io";
+import VueResource from "vue-resource";
+import { url } from "./utils/config";
 
-Vue.config.productionTip = false;
+// Socket config
+Vue.use(
+  new VueSocketIO({
+    debug: true,
+    connection: `${url}/video-chat`,
+    vuex: {
+      store, // Attach the store
+      actionPrefix: "SOCKET_",
+      mutationPrefix: "SOCKET_"
+    }
+  })
+);
 
+// Vue resource for http
+Vue.use(VueResource);
+
+// Vue instance
 new Vue({
   router,
-  store,
+  store, // Attach the store
   render: h => h(App)
 }).$mount("#app");
